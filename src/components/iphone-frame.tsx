147863357ac272e9e53f1946d3f44a1229e0edc9
Tarun j <tarun.reptile@gmail.com>
2025-12-02 10:35:08 +0000

    remove this dark oval shape div  (_for element div_)

--- src/components/iphone-frame.tsx
+++ src/components/iphone-frame.tsx
@@ -15,15 +15,10 @@ export function IPhoneFrame({ children, className }: IPhoneFrameProps) {
         className
       )}
     >
-      <div className="absolute left-1/2 top-0 h-4 w-40 -translate-x-1/2 rounded-b-lg bg-neutral-900"></div>
-      <div className="absolute -left-1 top-20 h-16 w-1 rounded-l-lg bg-neutral-900"></div>
-      <div className="absolute -left-1 top-48 h-8 w-1 rounded-l-lg bg-neutral-900"></div>
-      <div className="absolute -right-1 top-32 h-24 w-1 rounded-r-lg bg-neutral-900"></div>
       <div className="h-full w-full overflow-hidden rounded-[40px] bg-black">
         <div className="absolute inset-x-0 top-0 z-10 px-6 pt-2">
           <div className="flex items-center justify-between text-white">
             <span className="text-xs font-bold">9:41</span>
-            <div className="absolute left-1/2 h-7 w-32 -translate-x-1/2 rounded-full bg-black"></div>
             <div className="flex items-center gap-1">
               <Signal size={14} />
               <Wifi size={14} />
