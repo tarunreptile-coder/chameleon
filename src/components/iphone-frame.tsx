8d8e900f5d045cb4b61ff547c38c025fb7b94ba8
Tarun j <tarun.reptile@gmail.com>
2025-12-02 10:31:44 +0000

    In Iphone frame have fixed notification bar on top

--- src/components/iphone-frame.tsx
+++ src/components/iphone-frame.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from "react";
 import { cn } from "@/lib/utils";
+import { Wifi, Battery, Signal } from "lucide-react";
 
 type IPhoneFrameProps = {
   children: ReactNode;
@@ -19,7 +20,17 @@ export function IPhoneFrame({ children, className }: IPhoneFrameProps) {
       <div className="absolute -left-1 top-48 h-8 w-1 rounded-l-lg bg-neutral-900"></div>
       <div className="absolute -right-1 top-32 h-24 w-1 rounded-r-lg bg-neutral-900"></div>
       <div className="h-full w-full overflow-hidden rounded-[40px] bg-black">
-        <div className="absolute left-1/2 top-4 z-10 h-7 w-32 -translate-x-1/2 rounded-full bg-black"></div>
+        <div className="absolute inset-x-0 top-0 z-10 px-6 pt-2">
+          <div className="flex items-center justify-between text-white">
+            <span className="text-xs font-bold">9:41</span>
+            <div className="absolute left-1/2 h-7 w-32 -translate-x-1/2 rounded-full bg-black"></div>
+            <div className="flex items-center gap-1">
+              <Signal size={14} />
+              <Wifi size={14} />
+              <Battery size={18} />
+            </div>
+          </div>
+        </div>
         <div className="dark h-full w-full">{children}</div>
       </div>
     </div>
